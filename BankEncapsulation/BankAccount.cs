2a19746644e03415ed33de5c07c3da9c92b114d1
MadScientist586 <mr.scientist.knight@gmail.com>
2022-09-23 14:25:58 -0500

    first commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double Balance = 0;
+        public void Deposit(double userDepositAnswer)
+        {
+            Balance += userDepositAnswer;
+        }
+        public void Withdraw(double amountToWithdraw)
+        {
+            Balance -= amountToWithdraw;
+        }
+        public double GetBalance()
+        {
+            return Balance;
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,11 @@
+﻿using BankEncapsulation;
+BankAccount NewAccount = new BankAccount();
+NewAccount.Deposit(10000);
+Console.WriteLine($"You have deposited $10000. Your balance is now ${NewAccount.GetBalance()}");
+NewAccount.Deposit(500);
+Console.WriteLine($"You have deposited $500. Your balance is now ${NewAccount.GetBalance()}");
+NewAccount.Withdraw(900);
+Console.WriteLine($"You have withdrawn $900. Your balance is now ${NewAccount.GetBalance()}");
+
+
+
